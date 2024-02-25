ecd9dd11a27f85f872f5691c276fbd2c9aaceef2
SirBeho <benjamin.tavarez.98@gmail.com>
2024-02-25 17:47:18 -0400

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use tokio_postgres::{NoTls, Error};
+use tokio_postgres::{NoTls, Error,Client};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     // Configuración de la conexión a la base de datos
     let (client, connection) = tokio_postgres::connect(
-        "host=postgres-container user=benjamin password=1192141 dbname=tienda_db port=6001",
+        "host=postgres-container user=benjamin password=1192141 dbname=tienda_db port=5432",
         NoTls,
     )
     .await?;
@@ -16,16 +16,24 @@ async fn main() -> Result<(), Error> {
         }
     });
 
+    if let Err(e) = llamar_datos(&client).await {
+        eprintln!("Error al llamar datos: {}", e);
+    }
+
     
-    // Ejemplo de consulta
-    let rows = client.query("SELECT id_cliente, nombre FROM cliente", &[]).await?;
-    
-    // Procesar resultados
+    Ok(())
+}
+
+async fn llamar_datos(client: &Client) -> Result<(), Error> {
+
+    let rows = client.query("SELECT id_cliente,nombre, correo , telefono FROM cliente", &[]).await?;
+
     for row in rows {
         let id_cliente: i32 = row.get(0);
         let nombre: &str = row.get(1);
-        println!("ID: {}, Nombre: {}", id_cliente, nombre);
+        let correo: &str = row.get(2);
+        let telefono: &str = row.get(3);
+        println!("ID: {}, Nombre: {}, Correo: {}, Telefono: {}", id_cliente, nombre, correo, telefono);
     }
-
-    Ok(())
+       Ok(())
 }
